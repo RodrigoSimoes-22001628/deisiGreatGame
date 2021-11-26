7f8665ab78704f52c4c8dcdb4a9d82033c491268
Rodrigo Simões -a22001628 <a22001628@alunos.ulht.pt>
2021-11-26 17:02:24 +0000

    Adicionei imagens

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -96,6 +96,8 @@ public class GameManager {
         createInitialBoard(playerInfo,worldSize);
         ferramentas.clear();
         abismos.clear();
+        HashSet<Integer> abismosRepetidos = new HashSet<>();
+        HashSet<Integer> ferramentasRepetidas = new HashSet<>();
         if (!createInitialBoard(playerInfo, worldSize)){
             return false;
         }
@@ -105,24 +107,30 @@ public class GameManager {
         for(int i = 0; i < abyssesAndTools.length; i++){
             if (Integer.parseInt(abyssesAndTools[i][0]) == 0){
                 if (Integer.parseInt(abyssesAndTools[i][1]) < 0 || Integer.parseInt(abyssesAndTools[i][1]) > 9
-                        ||abyssesAndTools[i][2].equals("")){ //o  id tem de ser entre 0..9 e o título não pode ser vazio
+                    ||abyssesAndTools[i][2].equals("")|| abismosRepetidos.contains(Integer.parseInt(abyssesAndTools[i][1]))){ //o  id tem de ser entre 0..9 e o título não pode ser vazio
                     return false;
                 }
+
                 abismo.setId(Integer.parseInt(abyssesAndTools[i][1])); //adiciono o id
+                abismosRepetidos.add(Integer.parseInt(abyssesAndTools[i][1])); //evita ids repetidos abismos
                 abismo.setTitulo(abismoPorId(Integer.parseInt(abyssesAndTools[i][1]))); //adiciono o titulo correspondente ao id
                 abismo.setPosicao(Integer.parseInt(abyssesAndTools[i][2])); //adiciono a posicao
                 abismos.add(abismo); //adicionar na lista
+
             }else if (Integer.parseInt(abyssesAndTools[i][0]) == 1){
                 if (Integer.parseInt(abyssesAndTools[i][1]) < 0 || Integer.parseInt(abyssesAndTools[i][1]) > 5
-                        ||abyssesAndTools[i][2].equals("")) { //o  id tem de ser entre 0..9 e o título não pode ser vazio
+                        ||abyssesAndTools[i][2].equals("") || ferramentasRepetidas.contains(Integer.parseInt(abyssesAndTools[i][1]))) { //o  id tem de ser entre 0..9 e o título não pode ser vazio
                     return false;
                 }
-                    ferramenta.setId(Integer.parseInt(abyssesAndTools[i][1])); //adiciono o id
+
+                ferramenta.setId(Integer.parseInt(abyssesAndTools[i][1])); //adiciono o id
+                ferramentasRepetidas.add(Integer.parseInt(abyssesAndTools[i][1])); //evita ids repetidos ferramentas
                 ferramenta.setTitulo(ferramentaPorId(Integer.parseInt(abyssesAndTools[i][1]))); //adiciono o titulo correspondente ao id
                 ferramenta.setPosicao(Integer.parseInt(abyssesAndTools[i][2])); //adiciono a posicao
                 ferramentas.add(ferramenta); //adicionar na lista
             }
         }
+
         ferramentas.sort(Comparator.comparingInt(Ferramenta::getId));
         abismos.sort(Comparator.comparingInt(Abismo::getId));
         return true;
@@ -157,31 +165,28 @@ public class GameManager {
     }
 
     public String getImagePng(int position) {
-        StringBuilder imprimir = new StringBuilder();
+
         if (position > tamanhoTabuleiro) {
             return null;
         }
 
-        for (Abismo abismo : abismos){ //Reformular colocar por id
-            if (abismo.getPosicao() == position){
-               imprimir.append("abismo").append(abismo.getId()).append(".png");
+        for (Abismo abismo : abismos) { //Reformular colocar por id
+            if (abismo.getPosicao() == position) {
+                return "abismo" + abismo.getId() + ".png";
             }
         }
 
         for (Ferramenta ferramenta : ferramentas){
             if (ferramenta.getPosicao() == position){
-                imprimir.append("ferramenta").append(ferramenta.getId()).append(".png");
+                return "ferramenta"+ferramenta.getId()+".png";
             }
         }
 
         if (position == tamanhoTabuleiro) {
-            imprimir.append("glory.png");
+            return "glory.png";
         }
-        if (imprimir.toString().equals("")) {
-            return null;
-        }else {
-            return imprimir.toString().toString();
-        }
+
+        return null;
     }
 
     public List<Programmer> getProgrammers(boolean includeDefeated){
